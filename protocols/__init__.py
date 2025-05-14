2a025db120bb839d916d484a856f7ae24359dfd5
Lucas Monteiro Mobile <124006000+lucasmobileit@users.noreply.github.com>
2025-05-13 23:38:02 -0300

    Create __init__.py

---
+++ protocols/__init__.py
@@ -0,0 +1,3 @@
+from .active_enum import active_enum_tcp, active_enum_udp, validate_inputs, create_packet
+from .banner_grabbing import grab_banner, PORT_COMMANDS
+from .host_discovery_module import discover_hosts
