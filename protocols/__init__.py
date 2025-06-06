bbf62411779f462238c95ab9d0476b1c933c7e89
Lucas Monteiro Mobile <124006000+lucasmobileit@users.noreply.github.com>
2025-06-06 01:38:19 -0300

    Delete protocols directory

--- protocols/__init__.py
+++
@@ -1,3 +0,0 @@
-from .active_enum import active_enum_tcp, active_enum_udp, validate_inputs, create_packet
-from .banner_grabbing import grab_banner, PORT_COMMANDS
-from .host_discovery_module import discover_hosts
