90874a539c96c31d76c34b62a4608bb1f45493a8
Lucas Monteiro Mobile <124006000+lucasmobileit@users.noreply.github.com>
2025-06-12 09:46:05 -0300

    Update utils.py

--- application/utils.py
+++ application/utils.py
@@ -23,16 +23,12 @@ def extract_version(service_name, details_str):
     service_name_upper = service_name.upper()
     if not original_details:
         return None
-    if "POSTGRESQL" in service_name_upper:
-        m = re.search(r'(\d+\.\d+\.\d+)(?:\s*-\s*(\d+\.\d+\.\d+))?', original_details)
-        if m:
-            version = m.group(2) if m.group(2) else m.group(1)
-    elif "FTP" in service_name_upper:
-        m = re.search(r"vsFTPd\s+([\w\d\.\-]+)", original_details, re.IGNORECASE)
+    if "SSH" in service_name_upper:
+        m = re.search(r"OpenSSH[_\s]([\w\d\.\-p]+)", original_details, re.IGNORECASE)
         if m:
             version = m.group(1)
-    elif "SSH" in service_name_upper:
-        m = re.search(r"OpenSSH_([\w\d\.\-p]+)", original_details, re.IGNORECASE)
+    elif "FTP" in service_name_upper:
+        m = re.search(r"vsFTPd\s+([\w\d\.\-]+)", original_details, re.IGNORECASE)
         if m:
             version = m.group(1)
     elif "HTTP" in service_name_upper:
@@ -47,6 +43,10 @@ def extract_version(service_name, details_str):
         m = re.search(r"RFB\s+([\d\.]+)", original_details, re.IGNORECASE)
         if m:
             version = m.group(1)
+    elif "POSTGRESQL" in service_name_upper:
+        m = re.search(r'(\d+\.\d+\.\d+)(?:\s*-\s*(\d+\.\d+\.\d+))?', original_details)
+        if m:
+            version = m.group(2) if m.group(2) else m.group(1)
     return version
 
 def validate_input(target):
